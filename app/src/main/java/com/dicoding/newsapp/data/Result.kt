4ee6df8884ab46f6bbd982f64c024e41c87b4ad4
Gilang <gilangnurrizki173@gmail.com>
2024-10-01 21:07:04 +0700

    update

--- app/src/main/java/com/dicoding/newsapp/data/Result.kt
+++ app/src/main/java/com/dicoding/newsapp/data/Result.kt
@@ -1,4 +1,10 @@
 package com.dicoding.newsapp.data
 
-class Result {
+import kotlin.Result
+
+sealed class Result<out R> private constructor(){
+    data class Success<out T>(val data: T) : com.dicoding.newsapp.data.Result<T>()
+    data class Error(val error: String) : com.dicoding.newsapp.data.Result<Nothing>()
+    object Loading : com.dicoding.newsapp.data.Result<Nothing>()
+
 }
